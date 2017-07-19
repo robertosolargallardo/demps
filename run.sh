9795a8f27b2a9d6e6b18e507aab55f8113bb07df
Roberto Solar <roberto.solar.gallardo@gmail.com>
2017-07-19 15:16:19 -0400

    last update

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-time ./sim -m input/valparaiso.osrm -s input/settings.json -i input/initial_zones.json -r input/reference_zones.json -p input/reference_point.json
+time ./sim -m /home/rsolar/mobility_models/osm-data/valparaiso.osrm -s input/settings.json -i input/initial_zones.json -r input/reference_zones.json -p input/reference_point.json
