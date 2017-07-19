5f9373a92b8f78864a48db06ad70a9f2ae36c5f6
Roberto Solar <roberto.solar.gallardo@gmail.com>
2017-07-19 12:17:47 -0400

    updating input files

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-time ./sim -m /home/rsolar/mobility_models/osm-data/valparaiso.osrm -s settings.json -i initial_zones.json -r reference_zones.json -p reference_point.json
+time ./sim -m input/valparaiso.osrm -s input/settings.json -i input/initial_zones.json -r input/reference_zones.json -p input/reference_point.json
